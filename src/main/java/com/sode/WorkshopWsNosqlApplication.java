c09335028f936b883f3a95d1a9bafea91f3ff900
liam <iliamftw2013@gmail.com>
2025-05-06 08:36:34 -0300

    Init: project / repo

---
+++ src/main/java/com/sode/WorkshopWsNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.sode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopWsNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopWsNosqlApplication.class, args);
+	}
+
+}
